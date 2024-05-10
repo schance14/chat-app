a63d7fe930e6c772ef2a7ab1e36a3ab04a0155b6
schance14 <schance.sd14@gmail.com>
2024-05-10 15:04:33 -0600

    integrate Firestore database to chat app

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // import the screens we want to navigate
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// initialize connection for Firestore
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 
@@ -10,6 +13,22 @@ import Chat from "./components/Chat";
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  //Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDqNpeQFV0rCjRC-dcOgNqvXS07u9gGx60",
+    authDomain: "chat-app-f5880.firebaseapp.com",
+    projectId: "chat-app-f5880",
+    storageBucket: "chat-app-f5880.appspot.com",
+    messagingSenderId: "527924331872",
+    appId: "1:527924331872:web:0d123b55d3fb06062f8a05",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
